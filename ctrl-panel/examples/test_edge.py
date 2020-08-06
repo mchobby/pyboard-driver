72b21e0a9ad69227659c730fc933eca0a597a471
dmeurisse <info@mchobby.be>
2020-08-06 15:40:31 +0200

    Ctrl-panel: update lib and examples

---
+++ ctrl-panel/examples/test_edge.py
@@ -0,0 +1,46 @@
+# advanced example utisng  Initialization test for NADHAT-CONTROL-PANEL
+#
+# See: https://github.com/mchobby/pyboard-driver/tree/master/ctrl-panel
+#
+from machine import I2C
+from cpanel import *
+import time
+
+i2c = I2C( 1, freq=2000000 )
+p = CtrlPanel( i2c )
+
+# Oled is FrameBuffer based
+# See https://docs.micropython.org/en/latest/library/framebuf.html
+#
+p.oled.text("Button Edge", 10,10, 1)
+p.oled.text("Counter !", 10,20, 1)
+# Dessiner un rectangle blanc - rect( x, y, w, h, c )
+
+p.oled.rect( 0, 0, 128, 64, 1 )
+p.oled.rect( 3, 3, 128-2*3, 64-2*3, 1 )
+p.oled.text("Press...", 10, 40)
+p.oled.show()
+
+# Convenient translation
+# SW2 = /Q_ON is not wired on the MCP23017 but on GPIO pin 28
+# REQ_OFF also managed by SW2 and wired on MCP23017.GPA3
+to_text = { SW1:'SW1',SW3:'SW3',SW4:'SW4',UP:'UP',DOWN:'DOWN',LEFT:'LEFT',RIGHT:'RIGHT',CLICK:'CLICK' }
+
+while True:
+	# Press the button for 10 secondes
+	print( '---------------------------------------------------' )
+	print( 'Collecting for 10 sec. Press the buttons')
+	t = time.time()
+	while (time.time()-t) < 10:
+		p.update()
+		#time.sleep_ms( 100 )
+
+	# Yo! Display the key pressed and the number of time it has been done.
+	print( '----------------------------------------------------' )
+	for key,name in to_text.items():
+		count = p.edges( key ) # read it will reset it to 0
+		if count > 0:
+			print( "%s pressed %i times" % (name,count) )
+
+	# Prepare to next round
+	t = time.time()
--- ctrl-panel/lib/cpanel.py
+++ ctrl-panel/lib/cpanel.py
@@ -43,11 +43,67 @@ LEFT = 13
 RIGHT= 10
 CLICK= 12 # Labeled CTR
 
+class MCPEdge( MCP23017 ):
+	""" Class managing and signaling the state change """
+
+	def __init__(self, i2c, address=0x21, falling_edge=True ):
+		super().__init__(i2c,address)
+		self.falling_edge = falling_edge # Falling edge detection
+		self.last_gpio = bytearray(self.gpio_bytes) # retain last GPIO reading
+		self.edge_count = bytearray(self.NUM_GPIO) # Count the number of falling edges since last has_changed( pin ) read
+		self.edge_gpios = []
+
+	def old_input( self, pin ):
+		# return the Old Status of a pin
+		return (self.last_gpio[int(pin/8)] & 1 << (int(pin%8))) > 0
+
+	def is_input( self, pin ):
+		# Check if a pin is configured as Input
+		return self.iodir[int(pin/8)] & (1 << int(pin%8)) > 0
+
+	def edge_begin( self, edge_gpios ):
+		""" Call it after initialization of GPIO In/Out to collect last values of the list of GPIOs to monitor for EDGES. """
+		assert type(edge_gpios) is list, "edge_gpios must be a list"
+
+		self.edge_gpios = edge_gpios
+		self.read_gpio()
+		for i in range(self.gpio_bytes):
+			self.last_gpio[i] = self.gpio[i]
+
+	def update( self ):
+		# Call it after a read_gpio from main code
+
+		# Check if at least one change for input pins
+		#for item in [ (pin,self.input(pin,read=False),self.old_input(pin)) for pin in range(self.NUM_GPIO) if self.is_input(pin)]:
+		#	print(item)
+
+		if not any( [self.input(pin,read=False) != self.old_input(pin) for pin in self.edge_gpios if self.is_input(pin)] ):
+			return
+		# debouncing - only process updates if last check reveal a difference
+		time.sleep_ms(10)
+		self.read_gpio()
+		for pin in  self.edge_gpios:
+			if self.is_input(pin) and (self.input(pin,read=False) != self.old_input(pin)):
+				# is falling edge
+				if self.falling_edge and (self.old_input(pin) and not(self.input(pin,read=False)) ):
+					self.edge_count[pin] += 1
+				elif not(self.falling_edge) and ( not(self.old_input(pin)) and self.input(pin,read=False) ):
+					self.edge_count[pin] += 1
+		for i in range(self.gpio_bytes):
+			self.last_gpio[i] = self.gpio[i]
+
+	def edges( self, pin ):
+		""" Return the number of edge counts for a pin then reset it """
+		assert pin in self.edge_gpios, "%s not in edge_gpios" % pin
+		r = self.edge_count[pin]
+		self.edge_count[pin] = 0
+		return r
+
 class CtrlPanel:
 	def __init__( self, i2c ):
 		self.i2c = i2c
 
-		self._mcp = MCP23017( i2c, address=0x21 )
+		self._mcp = MCPEdge( i2c, address=0x21, falling_edge=True )
 		# Configure PinOut
 		#   GPA0..GPA3 are input
 		for pin in range( 0, 4 ):
@@ -61,7 +117,7 @@ class CtrlPanel:
 		#  GPB0..GPB7 are input (for the buttons)
 		for pin in range( 8, 16 ):
 			self._mcp.setup( pin, Pin.IN )
-
+		self._mcp.edge_begin( [SW1, SW3, SW4, UP, DOWN, LEFT, RIGHT, CLICK] ) # Initialize Edge counter
 		self.oled_reset()
 
 		self.oled = SSD1306_I2C(width=128, height=64, i2c=i2c, addr=0x3c, external_vcc=True)
@@ -76,10 +132,14 @@ class CtrlPanel:
 	def update( self ):
 		# read the GPIO
 		self._mcp.read_gpio() # Just read all the GPIOs (2 bytes)
+		self._mcp.update() # Update edges counters
 
 	def is_down( self, btn_gpio ):
 		# check if the button is down
 		return self._mcp.input(btn_gpio, read=False)==False # Low when pressed, Do not reread
+
+	def edges( self, btn_gpio ):
+		return self._mcp.edges(btn_gpio)
 
 	@property
 	def request_off( self ):
