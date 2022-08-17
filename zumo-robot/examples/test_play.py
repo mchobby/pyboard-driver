c5b1f331341551446ce759427a2cbbafbc69f487
dmeurisse <info@mchobby.be>
2022-08-17 22:16:54 +0200

    Move Zumo Robot to micropython-zumo-robot repository

--- zumo-robot/examples/test_play.py
+++
@@ -1,9 +0,0 @@
-""" test_play.py - just play a note
-
-See project source @ https://github.com/mchobby/pyboard-driver/tree/master/zumo-robot
-
-23 jul 2021 - domeu - initial writing
-"""
-from zumoshield import ZumoShield
-zumo = ZumoShield()
-zumo.buzzer.play(">g32>>c32")
